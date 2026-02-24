a89a3dcf6dc042cd3a1753da4664fdeb3913ceae
ujwala-gaddam <gaddamujwala24@gmail.com>
2026-02-24 23:30:55 +0530

    learn about productInput

--- src/com/java/fundamentals/ProductInput.java
+++ src/com/java/fundamentals/ProductInput.java
@@ -1,33 +1,46 @@
 package com.java.fundamentals;
+
 import java.util.Scanner;
 
 public class ProductInput {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter productId:");
-        byte product_id = scanner.nextByte();//
-        System.out.println("Enter name");
+
+        System.out.println("Enter Product ID : ");
+        byte id = scanner.nextByte();
+
+        System.out.println("Enter Product Name : ");
         String productName = scanner.next();
-        System.out.println("Enter manufacturedYear");
-        Short manufactured_year = scanner.nextShort();
-        System.out.println("Enter maxRetailPrice");
+
+        System.out.println("Enter Manufactured Year : ");
+        short manufacturedYear = scanner.nextShort();;
+
+        System.out.println("Enter Max Retail Price : ");
         int maxRetailPrice = scanner.nextInt();
-        System.out.println("Enter discountPercentage");
-        float discountPercentage= scanner.nextFloat();
-        System.out.println("finalPrice");
+
+        System.out.println("Enter Discount Percentage : ");
+        float discountPercentage = scanner.nextFloat();
+
+        System.out.println("Enter Product Final Price : ");
         double finalPrice = scanner.nextDouble();
-        System.out.println("Enter category");
-        char category =scanner.next().charAt(0);
-        System.out.println("is product Avaliabe(true/false)");
-        boolean productAvaliable = scanner.nextBoolean();
-        System.out.println("Product_id:"+ product_id);
-        System.out.println("productName:"+ productName);
-        System.out.println("manufacturedYear:"+ manufactured_year);
-        System.out.println("maxRetailprice:"+ maxRetailPrice);
-        System.out.println("Discount Percentage:"+ discountPercentage);
-        System.out.println("finalPrice:"+ finalPrice);
-        System.out.println("category:"+ category);
-        System.out.println("productAvailable:"+ productAvaliable);
+
+        System.out.println("Enter Product Category : ");
+        char productCategory = scanner.next().charAt(0);
+
+        System.out.println("is Product Available (true/flase) : ");
+        boolean isAvailable = scanner.nextBoolean();
+
+        System.out.println("Product ID : " + id);
+        System.out.println("Product Name : " + productName);
+        System.out.println("Manufactured Year : " + manufacturedYear);
+        System.out.println("Max Retail Price : " + maxRetailPrice);
+        System.out.println("Discount Percentage : " + discountPercentage);
+        System.out.println("Product Final Price : " + finalPrice);
+        System.out.println("Product Category : " + productCategory);
+        System.out.println("is Product Available : " + isAvailable);
+
+
+
 
     }
 }
